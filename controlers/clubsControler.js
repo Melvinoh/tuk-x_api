127c31be264be5a41c7a21856aff9e1524701382
Melvinoh <murichumelvin@gmail.com>
2023-07-21 11:00:48 +0300

    satuk api

--- controlers/clubsControler.js
+++ controlers/clubsControler.js
@@ -24,3 +24,45 @@ export const getClubsID = (req, res) =>{
     })
 
 }
+export const clubRegistration = (req,res) => {
+    const {clubid , regno} = req.body;
+
+    if(!clubid || !regno ){
+        return res.status(400).json("all inputs are required")
+    };
+
+    const q = "SELECT * FROM `studentclubs` where StudentID = ?"
+    db.query(q,[regno], (err,data) =>{
+        if(err) return res.status(500).json(err)
+        if(data[0].ClubID === clubid){
+            return res.status(409).json("you are already a member of the club");
+        }
+        const values = [ regno, clubid]
+        const q = " INSERT into studentclubs (StudentID, ClubID) values (?)"
+
+        db.query(q, [values], (err,data)=>{
+            if(err) return res.status(500).json(err);
+                return res.status(200).json("registration submmited awaiting for procesing");
+        })
+    })
+    
+}
+export const myClubs = (req, res) =>{ 
+
+    const {regno} = req.body
+    if(!regno){
+        return res.status(400).json("kindly login inorder to view your clubs");
+    }
+    const q = "SELECT * FROM clubs_tb JOIN `studentclubs` ON clubs_tb.ClubsID = studentclubs.ClubID WHERE studentclubs.StudentID = ?";
+
+     db.query(q , [regno], (err,data) =>{
+
+        if(err) return res.status(500).json(err);
+        if(data.length === 0) return res.status(500).json(err)
+        return res.status(200).json(data);
+
+     })
+
+
+        
+}
--- routers/clubs.js
+++ routers/clubs.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { getClubs, getClubsID } from "../controlers/clubsControler.js";
+import { getClubs, getClubsID, clubRegistration,myClubs } from "../controlers/clubsControler.js";
 
 
 const router = Router();
 
 router.get("/getClubs", getClubs );
 router.get("/getClubsID/:id", getClubsID );
+router.post("/clubreg", clubRegistration );
+router.post("/myclubs", myClubs);
 
 
 export default router
