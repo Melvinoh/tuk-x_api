b0e9e0d19798776f51f14929fe44ed49a1006d38
Melvinoh <murichumelvin@gmail.com>
2023-07-25 01:47:16 +0300

    api update

--- controlers/postsControler.js
+++ controlers/postsControler.js
@@ -5,18 +5,18 @@ import { v4 as uuidv4 } from 'uuid';
 
 export const addPost = (req, res) =>{
     const token = req.cookies.accessToken;
+    console.log(token);
     if(!token) return res.status(401).json("not logged in")
-
     jwt.verify(token, "secretKey", (err,student) =>{
         if (err) return res.status(403).json("token not vallied");
 
-        const {message, clubID } = req.body
+        const {desc, clubid } = req.body
+        if(!clubid) return res.status(409).json("posting failed please try again");
         const StudentID = student.id;
         const PostID = uuidv4();
         const createdAt =  moment(Date.now()).format("YYYY-MM-DD HH:mm:ss");
         
-
-        const values = [PostID, clubID, StudentID, message, createdAt];
+        const values = [PostID, clubid, StudentID, desc, createdAt];
         console.log(PostID);
         console.log(StudentID);
        
@@ -34,7 +34,7 @@ export const getPosts = (req, res) =>{
     const token = req.cookies.accessToken;
    
     const {clubid} = req.body
-    const q = "SELECT p.*,st.regno,fname,sname,profile_pic FROM `posts_tb` AS p JOIN `students_tb` AS st ON (st.regno = p.StudentID) WHERE p.postCatID = ?"
+    const q = "SELECT p.*,st.regno,fname,sname,profile_pic FROM `posts_tb` AS p JOIN `students_tb` AS st ON (st.regno = p.StudentID) WHERE p.postCatID = ? ORDER BY p.createdAt DESC"
     db.query(q, [clubid], (err,data) =>{
         if (err) return res.status(500).json(err)
         return res.status(200).json(data);
